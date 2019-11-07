d16f45f06fbae6ec8a78cc9ca7b5b7db0413f139
Simon Willison <swillison@gmail.com>
2019-11-06 21:42:28 -0800

    sqlite-utils~=1.12.1
    To get the bugfix for insert_all([])

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         [console_scripts]
         healthkit-to-sqlite=healthkit_to_sqlite.cli:cli
     """,
-    install_requires=["sqlite-utils"],
+    install_requires=["sqlite-utils~=1.12.1"],
     extras_require={"test": ["pytest"]},
     tests_require=["healthkit-to-sqlite[test]"],
 )
